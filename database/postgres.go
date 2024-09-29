e2cc8b13c8f3fa2d2d67af3c01304361edff5cd5
r. ávila <avila.dev@outlook.com>
2024-09-28 22:31:36 -0300

    docs: implement comments

--- database/postgres.go
+++ database/postgres.go
@@ -9,24 +9,31 @@ import (
 )
 
 var (
+	// Singleton instance of the GORM database connection.
 	Postgres = func() *gorm.DB {
-		psql_props := config.GetPostgresConfig()
+		// Retrieve PostgreSQL properties from the configuration
+		psql_props := config.PostgresProperties
 
+		// Get the Data Source Name (DSN) for connecting to the database
 		dsn := psql_props.DSN
 
+		// Open a connection to the PostgreSQL database using GORM
 		db, err := gorm.Open(postgres.Open(dsn))
 
 		if err != nil {
 			log.Fatalf("failed to connect to database - %v", err)
 		}
 
+		// Automatically migrate the database schema
 		AutoMigrate(db)
 
-		return db
-	}
+		return db // Return the database instance
+	}()
 )
 
+// AutoMigrate handles the automatic migration of the database schema.
 func AutoMigrate(db *gorm.DB) {
+	// Perform the auto migration for all registered models
 	err := db.AutoMigrate()
 
 	if err != nil {
