33b9a0cc66eaa93339c7e922d8f8145dee86ea71
r. ávila <avila.dev@outlook.com>
2024-09-28 22:31:42 -0300

    docs: implement comments

--- database/postgres_test.go
+++ database/postgres_test.go
@@ -8,33 +8,39 @@ import (
 	"gorm.io/gorm"
 )
 
+// User represents a model for the user entity with a name.
 type User struct {
 	gorm.Model
 	Name string
 }
 
+// Test_PostgresConnection tests the connection to the PostgreSQL database and the ability to create a user.
 func Test_PostgresConnection(t *testing.T) {
 	// Asserts that application will load .env.test instead
 	application.Env.ActivateTestEnvironment()
 
-	// It's a good approach to start our Postgres'
-	// instance dedicated to unit tests.
+	// Retrieve the PostgreSQL database instance
 	//
-	// Just run task `make test_start` to do that.
-	psql := database.Postgres()
+	// It's a good approach to start our PostgreSQL instance dedicated to unit tests.
+	// Just run task `make test_start` to set up the test environment.
+	psql := database.Postgres
 
-	// To generate SQL schema
+	// Automatically migrate the User model to generate the SQL schema in the database.
 	psql.AutoMigrate(&User{})
 
+	// Create a new User instance for testing.
 	u := User{
 		Name: "test-name",
 	}
 
+	// Attempt to create the user in the database.
 	r := psql.Create(&u)
 
+	// Check for errors during the user creation process.
 	if err := r.Error; err != nil {
 		t.Error(err)
 	}
 
+	// Log the ID of the created user.
 	t.Logf("Created user's ID: %v", u.ID)
 }
