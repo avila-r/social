81ed77bd96ab8a33e8ea8ac037b073505824ff19
r. ávila <avila.dev@outlook.com>
2024-09-29 17:40:35 -0300

    docs: describe exported fields

--- internal/project_paths.go
+++ internal/project_paths.go
@@ -6,12 +6,15 @@ import (
 )
 
 var (
+	// Retrieves the caller's information (file, line, etc.) at runtime.
 	_, b, _, _ = runtime.Caller(0)
 
+	// RootPath represents the application's root directory path.
 	RootPath = filepath.Join(
 		filepath.Dir(b), "../",
 	)
 
+	// E2ePath represents the path to the end-to-end (E2E) tests directory.
 	E2ePath = filepath.Join(
 		filepath.Dir(b), "../e2e",
 	)
