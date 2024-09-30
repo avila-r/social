ffc91080f24cfb321a2e25816309591c4e79d6b0
r. ávila <avila.dev@outlook.com>
2024-09-30 16:28:04 -0300

    feat: create user handler

--- domain/users/handler.go
+++ domain/users/handler.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"github.com/avila-r/xgo/pkg/validator"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -23,9 +24,58 @@ func NewHandler(s *UserService) *UserHandler {
 }
 
 func (h *UserHandler) Route(r fiber.Router) {
-	r.Get("/", h.HandleHome)
-}
+	// Create user
+	r.Post("/", func(c *fiber.Ctx) error {
+		request := struct {
+			DisplayName string `json:"display_name" validate:"required,min=3,max=50"`
+			Email       string `json:"email"        validate:"required,email"`
+			Password    string `json:"password"     validate:"required,min=8"`
+		}{}
 
-func (h *UserHandler) HandleHome(c *fiber.Ctx) error {
-	return c.JSON("Hello from user routes")
+		if err := c.BodyParser(&request); err != nil {
+			return fiber.ErrBadRequest
+		}
+
+		if err := validator.Validate(r); err != nil {
+			response := validator.Map(err)
+
+			return c.Status(500).JSON(response)
+		}
+
+		u := &User{
+			DisplayName: request.DisplayName,
+			Email:       request.Email,
+			Password:    request.Password,
+		}
+
+		if err := h.Service.CreateUser(u); err != nil {
+			return err
+		}
+
+		return c.Status(201).JSON(u)
+	})
+
+	// List users
+	r.Get("/", func(c *fiber.Ctx) error {
+		response, err := h.Service.ListUsers()
+
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(response)
+	})
+
+	// Get by email
+	r.Get("/email/:email", func(c *fiber.Ctx) error {
+		email := c.Params("email")
+
+		response, err := h.Service.FindByEmail(email)
+
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(response)
+	})
 }
