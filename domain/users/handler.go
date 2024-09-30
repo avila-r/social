428fa483bb027c573303e5432c275a6247f69b7f
r. ávila <avila.dev@outlook.com>
2024-09-30 19:12:07 -0300

    refactor: send registration flow to /auth domain

--- domain/users/handler.go
+++ domain/users/handler.go
@@ -1,7 +1,6 @@
 package users
 
 import (
-	"github.com/avila-r/xgo/pkg/validator"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -24,37 +23,6 @@ func NewHandler(s *UserService) *UserHandler {
 }
 
 func (h *UserHandler) Route(r fiber.Router) {
-	// Create user
-	r.Post("/", func(c *fiber.Ctx) error {
-		request := struct {
-			DisplayName string `json:"display_name" validate:"required,min=3,max=50"`
-			Email       string `json:"email"        validate:"required,email"`
-			Password    string `json:"password"     validate:"required,min=8"`
-		}{}
-
-		if err := c.BodyParser(&request); err != nil {
-			return fiber.ErrBadRequest
-		}
-
-		if err := validator.Validate(r); err != nil {
-			response := validator.Map(err)
-
-			return c.Status(500).JSON(response)
-		}
-
-		u := &User{
-			DisplayName: request.DisplayName,
-			Email:       request.Email,
-			Password:    request.Password,
-		}
-
-		if err := h.Service.CreateUser(u); err != nil {
-			return err
-		}
-
-		return c.Status(201).JSON(u)
-	})
-
 	// List users
 	r.Get("/", func(c *fiber.Ctx) error {
 		response, err := h.Service.ListUsers()
