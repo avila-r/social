c7586291c81c41fb83eaeba0690ee3892372cf10
r. ávila <avila.dev@outlook.com>
2024-10-01 15:46:44 -0300

    fix: rm list endpoint

--- domain/users/handler.go
+++ domain/users/handler.go
@@ -23,17 +23,6 @@ func NewHandler(s *UserService) *UserHandler {
 }
 
 func (h *UserHandler) Route(r fiber.Router) {
-	// List users
-	r.Get("/", func(c *fiber.Ctx) error {
-		response, err := h.Service.ListUsers()
-
-		if err != nil {
-			return err
-		}
-
-		return c.JSON(response)
-	})
-
 	// Get by email
 	r.Get("/email/:email", func(c *fiber.Ctx) error {
 		email := c.Params("email")
